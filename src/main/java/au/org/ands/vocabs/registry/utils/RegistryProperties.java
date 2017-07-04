1c580834843d5cbcdbf27998c61233b7dd13b058
Richard Walker <Richard.Walker@ands.org.au>
2017-07-04 12:24:06 +1000

    Make RegistryProperties match ToolkitProperties
    The code in the RegistryProperties class reflected an older version of
    ToolkitProperties. Update it to match. In particular, use Apache
    Commons Configuration for parsing.

--- src/main/java/au/org/ands/vocabs/registry/utils/RegistryProperties.java
+++ src/main/java/au/org/ands/vocabs/registry/utils/RegistryProperties.java
@@ -6,7 +6,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.invoke.MethodHandles;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Enumeration;
@@ -17,12 +19,24 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.servlet.ServletContext;
 
+import org.apache.commons.configuration.AbstractFileConfiguration;
+import org.apache.commons.configuration.ConfigurationConverter;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import au.org.ands.vocabs.toolkit.utils.ApplicationContextListener;
 
-/** Utility class providing access to roles properties. */
+/** Utility class providing access to registry properties.
+ * Properties are <i>implemented</i> using Apache Commons Configuration,
+ * but <i>accessed</i> as though they were Java Properties.
+ * The reason for using Commons Configuration is to support its
+ * extra features. For now, the one extra feature taken advantage
+ * of is interpolation of variables. That is, a property value
+ * may include the value of another property using the
+ * <code>${...}</code> syntax.
+ */
 public final class RegistryProperties {
 
     /** Base name of the registry properties file, if provided inside the
@@ -43,10 +57,17 @@ public final class RegistryProperties {
     private static final String REGISTRY_DUMP_PROPERTY =
             "registryDumpProperties";
 
-    /** Properties object. After initialization, contains all
-     * properties loaded from
-     * the registry properties file and the version properties file. */
-    private static Properties props;
+    /** All loaded properties. After initialization, contains all
+     * properties loaded from the registry properties file.
+     * This is the "definitive" copy of the properties. The
+     * field {@link RegistryProperties#propsAsProperties} is derived
+     * from this. */
+    private static AbstractFileConfiguration props;
+
+    /** A copy of all loaded properties, derived from
+     * {@link RegistryProperties#props}. This field is used to provide
+     * a cached copy for quick access. */
+    private static Properties propsAsProperties;
 
     /** Logger for this class. */
     private static Logger logger;
@@ -60,20 +81,21 @@ public final class RegistryProperties {
                 MethodHandles.lookup().lookupClass());
     }
 
-    /** Get the registry properties. (Forces initialization of the properties,
-     * if that has not already happened.)
-     * @return The properties.
+    /** Get all of the registry properties, as a Properties object.
+     * Forces initialization of the properties,
+     * if that has not already happened.
+     * @return The registry properties, as a Properties object.
      */
     public static Properties getProperties() {
         if (props == null) {
             initProperties();
         }
-        return props;
+        return propsAsProperties;
     }
 
     /** Get the value of a registry property.
-     * (Forces initialization of the properties,
-     * if that has not already happened.)
+     * Forces initialization of the properties,
+     * if that has not already happened.
      * @param propName The name of the property to fetch.
      * @return The value of the property.
      */
@@ -81,13 +103,13 @@ public final class RegistryProperties {
         if (props == null) {
             initProperties();
         }
-        return props.getProperty(propName);
+        return props.getString(propName);
     }
 
     /** Get the value of a registry property. This version of the method
      * allows specifying a default value for the property, if one
-     * has not been specified. (Forces initialization of the properties,
-     * if that has not already happened.)
+     * has not been specified. Forces initialization of the properties,
+     * if that has not already happened.
      * @param propName The name of the property to fetch.
      * @param defaultValue A default value to use, if there is no
      * property with name propName.
@@ -98,7 +120,7 @@ public final class RegistryProperties {
         if (props == null) {
             initProperties();
         }
-        return props.getProperty(propName, defaultValue);
+        return props.getString(propName, defaultValue);
     }
 
     /** Initialize the registry properties. Load the user-specified
@@ -116,11 +138,23 @@ public final class RegistryProperties {
      * loads files relative to {@code WEB-INF/classes}; when running standalone
      * code, the class loader loads files relative to the current working
      * directory when the JVM was started.)
+     * Specifically to support testing in Arquillian, in which this code
+     * is executed <i>both</i> inside and outside a servlet container,
+     * a system property {@code REGISTRY_PROPS_FILE_CLIENT_MODE} is supported,
+     * e.g., using
+     * {@code -DREGISTRY_PROPS_FILE_CLIENT_MODE=conf/registry-h2.properties},
+     * whose value overrides that of {@code PROPS_FILE} when
+     * this code is run outside a servlet container.
      */
     private static void initProperties() {
         logger.debug("In RegistryProperties.initProperties()");
+
         // Initialize props here, before loading any values into it.
-        props = new Properties();
+        props = new PropertiesConfiguration();
+        // Don't allow the presence of a comma to create multiple
+        // instances of a property.
+        props.setListDelimiter((char) 0);
+
         // Get the ServletContext, if any. If running standalone code,
         // this will be null.
         ServletContext servletContext = null;
@@ -139,7 +173,7 @@ public final class RegistryProperties {
             // (E.g., see the main() method, and the RewriteCurrent class.)
             logger.info("servletContext is null. Standalone application?");
         }
-        // InputStream for reading roles.properties.
+        // InputStream for reading registry.properties.
         InputStream input = null;
         // Let user override the REGISTRY_PROPS_FILE setting
         // with the command line.
@@ -160,6 +194,18 @@ public final class RegistryProperties {
                     // No servlet context, so no resolution against
                     // a path. In this case, doesn't _have_ to be an
                     // absolute path, but easier if it is.
+
+                    // Support Arquillian testing, in which we need to
+                    // get the properties file from two quite different
+                    // places.
+                    String propsFileClientMode = System.getProperty(
+                            "REGISTRY_PROPS_FILE_CLIENT_MODE");
+                    if (propsFileClientMode != null) {
+                        logger.debug("Overriding properties file "
+                                + "for client mode");
+                        propsFile = propsFileClientMode;
+                    }
+
                     logger.debug("Getting properties from a file, "
                             + "without servlet context");
                     input = new FileInputStream(propsFile);
@@ -220,9 +266,15 @@ public final class RegistryProperties {
         }
         try {
             // load a properties file
-            props.load(input);
-        } catch (IOException ex) {
-            ex.printStackTrace();
+            props.load(new InputStreamReader(input,
+                    StandardCharsets.UTF_8));
+            // Support interpolation of properties using ${...} syntax.
+            props = (AbstractFileConfiguration)
+                    props.interpolatedConfiguration();
+            // And now convert into Properties format for cached access.
+            propsAsProperties = ConfigurationConverter.getProperties(props);
+        } catch (ConfigurationException ce) {
+            logger.error("Exception while loading property file", ce);
         } finally {
             if (input != null) {
                 try {
@@ -243,9 +295,9 @@ public final class RegistryProperties {
      * property name contains the word "password", its value
      * is not displayed. */
     private static void dumpProperties() {
-        Enumeration<?> e = props.propertyNames();
+        Enumeration<?> e = getProperties().propertyNames();
 
-        logger.info("All roles properties:");
+        logger.info("All registry properties:");
         while (e.hasMoreElements()) {
             String key = (String) e.nextElement();
             if (key.matches(".*password.*")) {
@@ -254,9 +306,8 @@ public final class RegistryProperties {
                 logger.info(key + " -- " + props.getProperty(key));
             }
         }
-        logger.info("End of roles properties.");
+        logger.info("End of registry properties.");
     }
-
 
     /** Main method for testing.
      * @param args Command-line arguments.
