fefa83e2f6a8f1ad74c78e48ed9dd2162c2568db
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2017-10-23 12:40:49 +1100

    Added some minor tests for registry versionDAO

--- src/test/java/au/org/ands/vocabs/toolkit/test/arquillian/AllArquillianTests.java
+++ src/test/java/au/org/ands/vocabs/toolkit/test/arquillian/AllArquillianTests.java
@@ -2,6 +2,7 @@
 
 package au.org.ands.vocabs.toolkit.test.arquillian;
 
+import static au.org.ands.vocabs.toolkit.test.utils.DatabaseSelector.REGISTRY;
 import static au.org.ands.vocabs.toolkit.test.utils.DatabaseSelector.TOOLKIT;
 
 import java.io.File;
@@ -19,6 +20,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status.Family;
 
+import au.org.ands.vocabs.registry.db.dao.VersionDAO;
+import au.org.ands.vocabs.registry.enums.VersionStatus;
 import org.apache.commons.io.FileUtils;
 import org.apache.solr.client.solrj.SolrClient;
 import org.dbunit.Assertion;
@@ -932,6 +935,72 @@ public class AllArquillianTests extends ArquillianBaseTest {
             vocabularyList = VocabularyDAO.getAllVocabulary();
         Assert.assertNotNull(vocabularyList);
         Assert.assertEquals(vocabularyList.size(), 0, "Empty list");
+    }
+
+    /** Test of {@link VersionDAO#getAllVersion()} ()}.
+     * This is just a sanity test to make sure that the registry
+     * code is included correctly.
+     * @throws DatabaseUnitException If a problem with DbUnit.
+     * @throws HibernateException If a problem getting the underlying
+     *          JDBC connection.
+     * @throws IOException If a problem getting test data for DbUnit,
+     *          or reading JSON from the correct and test output files.
+     * @throws SQLException If DbUnit has a problem performing
+     *           performing JDBC operations.*/
+    @Test
+    public final void testVersionDAOGetAllVersion()
+            throws DatabaseUnitException, SQLException, IOException {
+        logger.info("In testVersionDAOGetAllVersion");
+
+        ArquillianTestUtils.clearDatabase(REGISTRY);
+
+        List<au.org.ands.vocabs.registry.db.entity.Version>
+                versionList = VersionDAO.getAllVersion();
+        Assert.assertNotNull(versionList);
+        Assert.assertEquals(versionList.size(), 0, "should be empty");
+    }
+
+    /**
+     * @throws DatabaseUnitException If a problem with DbUnit.
+     * @throws HibernateException If a problem getting the underlying
+     *          JDBC connection.
+     * @throws IOException If a problem getting test data for DbUnit,
+     *          or reading JSON from the correct and test output files.
+     * @throws SQLException If DbUnit has a problem performing
+     *           performing JDBC operations.
+     */
+    @Test
+    public final void testVersionDAOAddVersion()
+            throws DatabaseUnitException, SQLException, IOException {
+        logger.info("In testVersionDAOGetAllVersion");
+
+        ArquillianTestUtils.clearDatabase(REGISTRY);
+
+        au.org.ands.vocabs.registry.db.entity.Version
+                version = new au.org.ands.vocabs.registry.db.entity.Version();
+        version.setVersionId(1);
+        version.setData("some data");
+        version.setModifiedBy("modified_by_role_id");
+        version.setEndDate(LocalDateTime.now());
+        version.setReleaseDate("release_date");
+        version.setSlug("version-slug");
+        version.setStatus(VersionStatus.CURRENT);
+        version.setVocabularyId(1);
+        version.setStartDate(LocalDateTime.now());
+        VersionDAO.saveVersion(version);
+
+        // has 1
+        List<au.org.ands.vocabs.registry.db.entity.Version>
+                versionList = VersionDAO.getAllVersion();
+        Assert.assertNotNull(versionList);
+        Assert.assertEquals(versionList.size(), 1, "saved 1");
+        logger.info("versionID: " + versionList.get(0).getId());
+
+        // get
+        au.org.ands.vocabs.registry.db.entity.Version
+                versionEntity = VersionDAO.getVersionById(1);
+        Assert.assertNotNull(versionEntity);
+        Assert.assertEquals(versionEntity.getSlug(), versionEntity.getSlug());
     }
 
     /* Solr. */
