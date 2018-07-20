ce91adbe40532b5cc368aefadf60c67bbd574a83
Richard Walker <Richard.Walker@ands.org.au>
2018-07-20 14:30:36 +1000

    Add a test of notification subsystem with system access points
    Add a test case of the notification subystem, that tests adding and
    removing system-generated access points, and changes to the value of
    the do-publish flag.

--- src/test/java/au/org/ands/vocabs/toolkit/test/arquillian/CollectSubscriptionsTests.java
+++ src/test/java/au/org/ands/vocabs/toolkit/test/arquillian/CollectSubscriptionsTests.java
@@ -230,7 +230,8 @@ public class CollectSubscriptionsTests extends ArquillianBaseTest {
     }
 
     /** A test of the {@link CollectSubscriptions} class that
-     * observes addition and deletion of access points of a version.
+     * observes addition and deletion of user-specified access points
+     * of a version.
      * @throws DatabaseUnitException If a problem with DbUnit.
      * @throws IOException If a problem getting test data for DbUnit,
      *          or reading JSON from the correct and test output files.
@@ -241,6 +242,21 @@ public class CollectSubscriptionsTests extends ArquillianBaseTest {
     public void testCollectSubscriptions5() throws
     DatabaseUnitException, IOException, SQLException {
         scriptCollectSubscriptions1("testCollectSubscriptions5",
+                NOW_TIME_1, NOW_TIME_3);
+    }
+
+    /** A test of the {@link CollectSubscriptions} class that
+     * observes addition and deletion of system access points of a version.
+     * @throws DatabaseUnitException If a problem with DbUnit.
+     * @throws IOException If a problem getting test data for DbUnit,
+     *          or reading JSON from the correct and test output files.
+     * @throws SQLException If DbUnit has a problem performing
+     *           performing JDBC operations.
+     */
+    @Test
+    public void testCollectSubscriptions6() throws
+    DatabaseUnitException, IOException, SQLException {
+        scriptCollectSubscriptions1("testCollectSubscriptions6",
                 NOW_TIME_1, NOW_TIME_3);
     }
 
