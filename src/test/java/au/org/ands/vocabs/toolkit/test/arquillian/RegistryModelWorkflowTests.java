ea4b0abfdf9ed99ad2544921c94ca2215a338466
Richard Walker <Richard.Walker@ands.org.au>
2020-03-10 16:15:21 +1100

    CC-2704 Add test for defect with re-importing on PP harvest delete
    Consider a vocabulary that is based on a PoolParty project, and that
    has an existing version such that:
    * do-poolparty-harvest="true"
    * do-import="true"
    * the version has a file access point
    
    Now update the version, but set do-poolparty-harvest="false".
    
    In this case, a Sesame import subtask must be run (together with the
    usual follow-up SesameInsertMetadata transform), to clear out the
    concept data that was previously harvested, and to re-import just the
    concept data from the file access point.  But that is not
    happening. No Sesame import subtask (with SesameInsertMetadata
    transform) is run.
    
    Add a test that shows this defective behaviour. For now, disable the
    test.

--- src/test/java/au/org/ands/vocabs/toolkit/test/arquillian/RegistryModelWorkflowTests.java
+++ src/test/java/au/org/ands/vocabs/toolkit/test/arquillian/RegistryModelWorkflowTests.java
@@ -1028,6 +1028,70 @@ public class RegistryModelWorkflowTests extends ArquillianBaseTest {
         ArquillianTestUtils.assertTempForTestHasFiles(testName, 6);
     }
 
+    /** Test of applying changes to the current instance of a vocabulary
+     * that does not have a draft instance.
+     * Vocabulary, Version, VocabularyRelatedEntity,
+     * AccessPoint, and VersionArtefact
+     * model elements are used. Workflow processing is applied.
+     * This test involves turning off the do-poolparty-harvest flag
+     * for a version, where the do-import flag is, and remains set.
+     * The version remains valid, because there is also a file
+     * access point. But we require that the Sesame import task be
+     * run, to have the effect of removing the vocabulary data that
+     * was previously harvested.
+     * This test currently fails. See CC-2704.
+     * @throws DatabaseUnitException If a problem with DbUnit.
+     * @throws IOException If a problem getting test data for DbUnit,
+     *          or reading JSON from the correct and test output files.
+     * @throws SQLException If DbUnit has a problem performing
+     *           performing JDBC operations.
+     * @throws JAXBException If a problem loading vocabulary data.
+     *  */
+    @SuppressWarnings("checkstyle:MagicNumber")
+    @Test(enabled = false)
+    public final void testApplyChangesCurrentVoVREVeAPVAW12() throws
+    DatabaseUnitException, IOException, SQLException, JAXBException {
+        String testName = CLASS_NAME_PREFIX
+                + "testApplyChangesCurrentVoVREVeAPVAW12";
+        ArquillianTestUtils.clearDatabase(ROLES);
+        ArquillianTestUtils.loadDbUnitTestFile(ROLES, testName);
+        ArquillianTestUtils.clearDatabase(REGISTRY);
+        ArquillianTestUtils.loadDbUnitTestFile(REGISTRY, testName);
+        ArquillianTestUtils.copyUploadsFilesForTest(testName);
+        ArquillianTestUtils.copyTempFilesForTest(testName);
+        ArquillianTestUtils.assertTempForTestHasFiles(testName, 9);
+        Vocabulary vocabulary = RegistryTestUtils.
+                getValidatedVocabularyFromFile(
+                "test/tests/" + testName + "/test-vocabulary.xml",
+                ValidationMode.UPDATE);
+        EntityManager em = null;
+        try {
+            em = DBContext.getEntityManager();
+            em.getTransaction().begin();
+            VocabularyModel vm = ModelMethods.createVocabularyModel(em, 1);
+            ModelMethods.applyChanges(vm, "TEST", nowTime1, vocabulary);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em != null) {
+                em.getTransaction().rollback();
+                throw e;
+            }
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+
+//        ArquillianTestUtils.exportFullDbUnitData(REGISTRY,
+//                testName + "-out.xml");
+        ArquillianTestUtils.
+        compareDatabaseCurrentAndExpectedContentsIgnoreTaskTimestamps(
+                REGISTRY,
+                "test/tests/" + testName + "/test-registry-results.xml");
+        // The same number of files as before.
+        ArquillianTestUtils.assertTempForTestHasFiles(testName, 9);
+    }
+
     /** Test of adding a draft to a vocabulary
      * that has an existing published instance, and then publishing
      * the same.
